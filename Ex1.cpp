c8c7b703c51ca3f1479b4c2328ce6e4a09aa65e8
llildin <132454924+llildin@users.noreply.github.com>
2026-02-25 09:03:28 +0900

    Initial commit

---
+++ Ex1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int Num1;
+	int Num2;
+
+	cin >> Num1;
+	cin >> Num2;
+
+	cout << "Num1 + Num2 = " << Num1 + Num2 << endl;
+	cout << "Num1 - Num2 = " << Num1 - Num2 << endl;
+	cout << "Num1 * Num2 = " << Num1 * Num2 << endl;
+	cout << "Num1 / Num2 = " << Num1 / Num2 << endl;
+	cout << "Num1 % Num2 = " << Num1 % Num2 << endl;
+
+	return 0;
+}
